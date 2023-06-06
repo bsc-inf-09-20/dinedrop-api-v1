b023f5720d490c3c36f978c0a7e4d29170c42556
bsc-inf-09-20 <bsc-inf-09-20@unima.ac.mw>
2023-06-06 15:09:06 +0200

    update

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,11 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, IsNumber } from "class-validator";
 
 export class CreateOrderDto{
 
     @IsString()
+    @ApiProperty()
     username: string;
+    
     @IsString()
+    @ApiProperty()
     meal: string;
+
     @IsNumber()
+    @ApiProperty()
     price: number;
 }
